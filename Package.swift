b00c8a3d526960436d7482658580103581b70d01
Jairon Terrero <jairon@passivelogic.com>
2025-12-23 08:59:47 -0700

    ff

--- Package.swift
+++ Package.swift
@@ -7,12 +7,12 @@ let packageName = "PowerSync"
 
 // Set this to the absolute path of your Kotlin SDK checkout if you want to use a local Kotlin
 // build. Also see docs/LocalBuild.md for details
-let localKotlinSdkOverride: String? = "/Users/jaironterrero/repos/0/powersync-kotlin"
-// let localKotlinSdkOverride: String? = nil
+// let localKotlinSdkOverride: String? = "/Users/jaironterrero/repos/0/powersync-kotlin"
+let localKotlinSdkOverride: String? = nil
 // Set this to the absolute path of your powersync-sqlite-core checkout if you want to use a
 // local build of the core extension.
-let localCoreExtension: String? = "/Users/jaironterrero/repos/0/powersync-sqlite-core"
-// let localCoreExtension: String? = nil
+// let localCoreExtension: String? = "/Users/jaironterrero/repos/0/powersync-sqlite-core"
+let localCoreExtension: String? = nil
 
 // Currently encryption is required for GRDB integration
 let encryption = true
